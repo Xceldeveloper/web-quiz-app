8b70ca0ca9234ba9b76db6478a4ea12b75b5b04d
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2020-11-17 14:23:47 +0100

    edits

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - xcelQuiz',
-    title: 'xcelQuiz',
+    titleTemplate: '%s - XcelQuiz',
+    title: 'XcelQuiz',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport',   content: 'width=device-width, initial-scale=1,user-scalable=no'},
